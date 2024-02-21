6821ae68e6362b4ed2172afb44b630aa97c30f32
upretymadan45 <upretymadan45@gmail.com>
2024-02-21 18:03:06 +0545

    Update sqlite.py

--- sqlite.py
+++ sqlite.py
@@ -1,52 +1,53 @@
-
 import sqlite3
 
-##connect to SQLite
-connection = sqlite3.connect("student.db")
+def createStudent():
+    connection = sqlite3.connect("student.db")
+    cursor = connection.cursor()
 
-#Create a cursor object to insert record,create table
-cursor = connection.cursor()
+    #create the table
+    table_info = """
+        Create table Student(Id Int, Name Varchar(50), Class varchar(50), Section varchar(50),Marks Int);
+    """
+    cursor.execute(table_info)
 
-##create the table
-# table_info = """
-#     Create table Student(Name Varchar(50), Class varchar(50), Section varchar(50),
-#     Marks Int);
-# """
+    #Insert students
+    cursor.execute("""Insert into Student values(1,'Jack','Data Science','A',90)""")
+    cursor.execute("""Insert into Student values(2,'Madan','Data Science','B',100)""")
+    cursor.execute("""Insert into Student values(3,'Matt','Data Science','A',86)""")
+    cursor.execute("""Insert into Student values(4,'Harry','DevOPS','A',50)""")
+    cursor.execute("""Insert into Student values(5,'Ben','DevOPS','A',35)""")
 
-# table_info = """
-#     Create table Fees(StudentId int, Month varchar(50), Amount decimal);
-# """
+    #Display records
+    print("The inserted records are")
+    data = cursor.execute("""Select * from student""")
+    for row in data:
+        print(row)
 
-table_info = """
-    Alter table Student add column Id int;
-"""
+    connection.commit()
+    connection.close()
 
-cursor.execute(table_info)
+def createFee():
+    connection = sqlite3.connect("student.db")
+    cursor = connection.cursor()
+    
+    table_info = """
+    Create table Fees(StudentId int, Month varchar(50), Amount decimal);
+    """
 
-# cursor.execute("""Insert into Student values('Manju','Data Science','A',90)""")
-# cursor.execute("""Insert into Student values('Madan','Data Science','B',100)""")
-# cursor.execute("""Insert into Student values('Ambika','Data Science','A',86)""")
-# cursor.execute("""Insert into Student values('Kushal','DevOPS','A',50)""")
-# cursor.execute("""Insert into Student values('Sangita','DevOPS','A',35)""")
+    cursor.execute("""Insert into Fees values(1,'Baishak',2000)""")
+    cursor.execute("""Insert into Fees values(1,'Jesth',3000)""")
+    cursor.execute("""Insert into Fees values(2,'Ashad',5000)""")
 
+    #Display records
+    print("The inserted records are")
+    data = cursor.execute("""Select * from Fees""")
+    for row in data:
+        print(row)
 
-# cursor.execute("""Insert into Fees values(1,'Baishak',2000)""")
-# cursor.execute("""Insert into Fees values(1,'Jesth',3000)""")
-# cursor.execute("""Insert into Fees values(2,'Ashad',5000)""")
+    connection.commit()
+    connection.close()
 
-cursor.execute("""Update Student set Id = 1 where name = 'Manju'""")
-cursor.execute("""Update Student set Id=2 where name='Madan'""")
-cursor.execute("""Update Student set Id=3 where name='Ambika'""")
-cursor.execute("""Update Student set Id=4 where name='Kushal'""")
-cursor.execute("""Update Student set Id=5 where name='Sangita'""")
+if __name__ == "__main__":
+    createStudent()
+    createFee()
 
-
-#Display all the records
-print("The inserted records are")
-data = cursor.execute("""Select * from student""")
-for row in data:
-    print(row)
-
-#Commit your changes to database
-connection.commit()
-connection.close()
