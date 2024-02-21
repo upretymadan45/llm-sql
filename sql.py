d958dacce2b79120fabb1b6e6d99192acdcfa270
upretymadan45 <upretymadan45@gmail.com>
2024-02-21 19:38:30 +0545

    Update sql.py

--- sql.py
+++ sql.py
@@ -1,69 +1,66 @@
 from dotenv import load_dotenv
-load_dotenv() ## load all the environment variables
+load_dotenv()
 
 import streamlit as st
-import os 
-import sqlite3
-
 import google.generativeai as genai
 
-##Configure GenAI Key
+import os
+import sqlite3
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-#Function to load google gemini model and provide queries as response
-
 def get_gemini_response(question,prompt):
-    model = genai.GenerativeModel('gemini-pro')
-    response = model.generate_content([prompt[0],question])
+    model = genai.GenerativeModel("gemini-pro")
+    response = model.generate_content([prompt,question])
     return response.text
 
-#Function to retrieve query from the database
-
 def read_sql_query(sql,db):
-    conn=sqlite3.connect(db)
-    cur=conn.cursor()
-    cur.execute(sql)
-    rows=cur.fetchall()
-    conn.commit()
-    conn.close()
-    for row in rows:
-        print(row)
-    return rows
+    forbidden = False
+    rows = []
+    
+    if contains_forbidden_words(sql):
+        forbidden = True
+    else:
+        conn=sqlite3.connect(db)
+        cur=conn.cursor()
+        cur.execute(sql)
+        rows=cur.fetchall()
+        conn.commit()
+        conn.close()
+        for row in rows:
+            print(row)
+    
+    return rows, forbidden
 
-#Define your prompt
+def contains_forbidden_words(sql):
+    forbidden_words = ["delete","alter","drop","truncate"]
+    for word in forbidden_words:
+        if word in sql.lower():
+            return True
+    return False
 
-prompt=[
-    """
-    You are an expert in converting English questions to SQL query!
-    The SQL database has the name STUDENT and has the following columns - Id, Name, Class,
-    Section \n\n For example, \n Example 1 - How many entries of records are present?,
-    the SQL command will be something like this Select Count(*) from Student;
-    \nExample 2 - Tell me all the students studying in Data Science class?,
-    the SQL command will be something like this Select * from student where
-    class="Data Science";
-    There is another table called Fees linked with student table using foreign key StudentId. One student can have multiple Fees. Fees table will have\n 
-    columns like - StudentId, Month and Amount.\n
-    also the sql code should not have ``` in the beginning or end and sql word in 
-    output
 
-    """
-]
+prompt="""
+Generate only SQL database query based on the English question. Do not generate any text other than SQL query.
+There is a Student database with two tables: Student and Fees. Student table has column Id, Name, Class,
+Section, Marks. Fees table has columns StudentId, Month, Amount.
+Do not include ``` at the start or end of query. Also do not put sql keyword in the query
+"""
 
-#Streamlit App
-st.set_page_config(page_title="I can Retrieve Any SQL query")
-st.header("Geminni App To Retrieve SQL Data")
+st.set_page_config(page_title="I can query your question")
+st.header("Sql query generator using Gemini Pro")
+question = st.text_input("Question ",key="input")
+submit=st.button("Ask Now")
 
-question=st.text_input("Input: ",key="input")
-
-submit=st.button("Ask the question")
-
-#If submit is clicked
 if submit:
-    response=get_gemini_response(question,prompt)
-    print(response)
-    response=read_sql_query(response,"student.db")
-    st.subheader("The Response is")
-    for row in response:
-        print(row)
-        st.header(row)
+    query=get_gemini_response(question,prompt)
+    print(query)
+    rows, forbidden=read_sql_query(query,"student.db")
+    st.subheader("The output is:")
+
+    if forbidden == True:
+        st.header("Could not execute delete or alter or drop or truncate query")
+    else:
+        for row in rows:
+            print(row)
+            st.header(row)
